b81c774bf5026b538434f0062b54e1448e55bd62
daniloBraj <165780340+daniloBraj@users.noreply.github.com>
2024-05-17 10:29:40 +0100

    added css and cross task on click

--- src/App.js
+++ src/App.js
@@ -1,53 +1,68 @@
-import { useState } from "react";
+
+import React, { useState } from "react";
 import Form from "./Form";
-import Task from "./Task";
+import "./main.css";
 
 const App = () => {
   const [taskInput, setTaskInput] = useState("");
-  const [task, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-  const handleClick = (index) => {
-    let storedTask = [...task];
-    storedTask.splice(index, 1);
-    setTask(storedTask);
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleToggleComplete = (index) => {
+    const updatedTasks = tasks.map((task, idx) => {
+      if (idx === index) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setTaskInput("");
-    setTask([...task, {task: taskInput}])
+    setTasks([...tasks, { task: taskInput, completed: false }]);
   };
 
-  const Task = (props) => {
-    return(
-        <div>
-            <h3>• {props.task}</h3>
-            <button onClick={props.handleClick}>Remove</button>
-        </div>
-    )
-  }
+  const Task = ({ task, index, onDelete, onToggleComplete }) => {
+    return (
+      <div>
+        <h3
+          className={`task ${task.completed ? "completed" : ""}`}
+          onClick={() => onToggleComplete(index)}
+        >
+          • {task.task}
+        </h3>
+        <button onClick={() => onDelete(index)}>Delete</button>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h1>To-Do List</h1>
+      <h3>CLICK ON TASK TO CROSS IT</h3>
       <Form
         handleSubmit={handleSubmit}
         taskInput={taskInput}
         setTaskInput={setTaskInput}
       />
-      {task.map((task, index) => {
-        return (
-          <Task
-            task={task.task}
-            key={index}
-            handleClick={() => handleClick(index)}
-          />
-        );
-      })}
+      {tasks.map((task, index) => (
+        <Task
+          task={task}
+          key={index}
+          index={index}
+          onDelete={handleDelete}
+          onToggleComplete={handleToggleComplete}
+        />
+      ))}
     </div>
   );
 };
-
-//
-
 
 export default App;
